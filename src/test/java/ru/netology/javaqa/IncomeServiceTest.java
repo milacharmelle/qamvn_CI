98bdec1566b7e747146f087cf02c761bffeec67b
Your Name <102684272+milacharmelle@users.noreply.github.com>
2022-09-26 15:34:36 +0500

    git commit

---
+++ src/test/java/ru/netology/javaqa/IncomeServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IncomeServiceTest {
+    @Test
+    public void ShouldNoIncome (){
+        IncomeService service = new IncomeService();
+        int age = 2;
+        int actual = service.calcIncome(age);
+        int expected = 0;
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShouldGetSalary(){
+        IncomeService service = new IncomeService();
+        int age = 50;
+        int actual = service.calcIncome(age);
+        int expected = 50_000;
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
